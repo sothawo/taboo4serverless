4f0b6648ecda85489b6ee475d5500412e1bccfd3
P.J.Meisch <pj.meisch@sothawo.com>
2018-12-23 06:25:34 +0100

    added serverless

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ the next version of my bookmarkservice in serverless usign AWS
 After OpenShift stopped its developer program where I had my taboo3 app running it's time again to change it.
 
 This version now will be build for running on AWS using Lambdas, API Gateway, S3 and DynamoDB.
+
+#  subdirectories
+
+## playground
+
+this is where I try things
+
+## backend
+
+the backend service with the lambdas and serverless configuration
+
+## frontend
+
+thw frontend single page app
---
+++ playground/test01/handler.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+function hello(mode) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(mode + ' executed successfully')
+        }, 2000)
+    });
+}
+
+module.exports.helloAsync = async (event, context) => {
+
+    var response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: await hello('helloAsync'),
+            input: event,
+        }),
+    };
+
+    return response;
+
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.helloSync = (event, context, callback) => {
+
+
+    hello('helloSync')
+        .then(result => {
+            var response = {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: result,
+                    input: event,
+                }),
+            };
+            callback(null, response)
+        })
+        .catch(e => {
+            callback(e, null)
+        });
+
+};
