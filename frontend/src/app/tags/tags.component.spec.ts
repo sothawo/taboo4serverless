08d03616d5a6f4a7177956579b962208eea3c4a3
P.J. Meisch <pj.meisch@sothawo.com>
2019-01-04 13:05:01 +0100

    added tests

--- frontend/src/app/tags/tags.component.spec.ts
+++ frontend/src/app/tags/tags.component.spec.ts
@@ -1,11 +1,14 @@
 import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
 import {TagsComponent} from "./tags.component";
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe("TagsComponent", () => {
     let component: TagsComponent;
     let fixture: ComponentFixture<TagsComponent>;
-    let componentElement: HTMLElement;
+    let debugElement: DebugElement;
+    let nativeElement: HTMLElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -18,7 +21,8 @@ describe("TagsComponent", () => {
         fixture = TestBed.createComponent(TagsComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-        componentElement = fixture.debugElement.nativeElement;
+        debugElement = fixture.debugElement;
+        nativeElement = debugElement.nativeElement;
     });
 
     it("should create", () => {
@@ -26,35 +30,47 @@ describe("TagsComponent", () => {
     });
 
     it("has a .card div", () => {
-        expect(componentElement.querySelector(".card")).toBeTruthy();
+        expect(nativeElement.querySelector(".card")).toBeTruthy();
     });
 
     it("is has a header that is displayed in a .card-header div", () => {
         component.header = "a big head";
         fixture.detectChanges();
         expect(component.header).toBeTruthy();
-        expect(componentElement.querySelector(".card-header").textContent).toBe(component.header);
+        expect(nativeElement.querySelector(".card-header").textContent).toBe(component.header);
     });
 
     it("has a .card-body", () => {
-        expect(componentElement.querySelector(".card-body")).toBeTruthy();
+        expect(nativeElement.querySelector(".card-body")).toBeTruthy();
     });
 
     describe("when using tags", () => {
         it("has no initial tags", () => {
-            expect(component.tags.length).toBe(0);
+            expect(component.tags.size).toBe(0);
             fixture.detectChanges();
-            expect(componentElement.querySelectorAll('button').length).toBe(0)
+            expect(nativeElement.querySelectorAll('button').length).toBe(0)
         });
-        it("has a button for each tag", () => {
-            component.tags = ["one", "two", "three"]
-            expect(component.tags.length).toBe(3);
+
+        it("has a button for each tag, sorted by tag", () => {
+            component.tags = new Set(["one", "two", "three"]);
             fixture.detectChanges();
-            let bottonElements = componentElement.querySelectorAll('button');
+            let bottonElements = nativeElement.querySelectorAll('button');
             expect(bottonElements.length).toBe(3)
             expect(bottonElements.item(0).textContent).toBe("one")
-            expect(bottonElements.item(1).textContent).toBe("two")
-            expect(bottonElements.item(2).textContent).toBe("three")
+            expect(bottonElements.item(1).textContent).toBe("three")
+            expect(bottonElements.item(2).textContent).toBe("two")
+        });
+
+        it("should emit an event with a tag name when clicking a button", () => {
+            component.tags = new Set(["click-me"]);
+            fixture.detectChanges();
+            let bottonDebugElement = debugElement.query(By.css("button"));
+            let selectedTag: string ="oops";
+            component.selected.subscribe((tag: string) => selectedTag = tag)
+
+            bottonDebugElement.triggerEventHandler("click", null);
+
+            expect(selectedTag).toBe("click-me");
         });
     });
 });
