46e894f98da49371cfacfaf02445e1ab6ee4d2af
P.J. Meisch <pj.meisch@sothawo.com>
2020-10-29 10:44:24 +0100

    add Logger

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from "./App.module.css"
-import {Header} from "./header/Header";
-import {TagList} from "./taglist/TagList";
-import {Logs} from "./logs/Logs";
-import {Bookmarks} from "./bookmarks/Bookmarks";
-import {Bookmark} from "./bookmarks/Bookmark";
-import {LogData, LogLevel} from "./logs/LogData";
-import {LocalStorage} from "./localstorage/LocalStorage";
-import {Settings, SettingsData} from "./settings/Settings";
+import styles from './App.module.css';
+import {Header} from './header/Header';
+import {TagList} from './taglist/TagList';
+import {Logs} from './logs/Logs';
+import {Bookmarks} from './bookmarks/Bookmarks';
+import {Bookmark} from './bookmarks/Bookmark';
+import {LogData, LogLevel} from './logs/LogData';
+import {LocalStorage} from './localstorage/LocalStorage';
+import {Settings, SettingsData} from './settings/Settings';
+import {Logger} from './logs/Logger';
 
 export interface AppProps {
     availableActive: boolean,
@@ -25,27 +26,27 @@ const initialAppProps: AppProps = {
     selectedActive: true,
     logsActive: false,
     showSettings: false,
-    bookmarks: [new Bookmark("42", "https://www.sothawo.com", "sothawo-website", ["cool", "unbelievable"])],
+    bookmarks: [new Bookmark('42', 'https://www.sothawo.com', 'sothawo-website', ['cool', 'unbelievable'])],
     logData: [
-        new LogData(LogLevel.DEBUG, "some debug message"),
-        new LogData(LogLevel.INFO, {foo: "info", level: 7}),
-        new LogData(LogLevel.WARN, "some warn message"),
-        new LogData(LogLevel.ERROR, {foo: "bar", level: 42})
+        new LogData(LogLevel.DEBUG, 'some debug message'),
+        new LogData(LogLevel.INFO, {foo: 'info', level: 7}),
+        new LogData(LogLevel.WARN, 'some warn message'),
+        new LogData(LogLevel.ERROR, {foo: 'bar', level: 42})
     ],
     localStorage: new LocalStorage()
-}
+};
 
 
 const toggleComponentAvailability = function (props: AppProps, component: string): AppProps {
     let newProps = {...props};
     switch (component) {
-        case "available":
+        case 'available':
             newProps.availableActive = !newProps.availableActive;
-            break
-        case "selected":
+            break;
+        case 'selected':
             newProps.selectedActive = !newProps.selectedActive;
             break;
-        case "logs":
+        case 'logs':
             newProps.logsActive = !newProps.logsActive;
             break;
         default:
@@ -56,54 +57,62 @@ const toggleComponentAvailability = function (props: AppProps, component: string
 
 export function App() {
 
-    const [props, setProps] = useState<AppProps>(initialAppProps)
+    const [props, setProps] = useState<AppProps>(initialAppProps);
 
     const navbarHandler = (id: string) => {
-        console.log(`Header event with id: ${id}`)
+        console.log(`Header event with id: ${id}`);
         switch (id) {
-            case "settings":
-                setProps({...props, showSettings: true})
+            case 'settings':
+                setProps({...props, showSettings: true});
                 break;
             default:
                 setProps(toggleComponentAvailability(props, id));
                 break;
         }
-    }
+    };
 
     const selectTag = (tag: string) => {
-    }
+    };
     const deselectTag = (tag: string) => {
-    }
+    };
     const editBookmark = (id: string) => {
-    }
+    };
     const deleteBookmark = (id: string) => {
-    }
+    };
 
-    const addLog = (log: LogData) => props.logData.push(log);
-    const clearLogs = () => setProps({...props, logData: []})
+    const logger = new Logger({
+        log: (logData: LogData) => {
+            props.logData.push(logData);
+            setProps({...props, logData: props.logData});
+        }
+    });
 
-    const handleSettingsClose = () => setProps({...props, showSettings: false})
+    const clearLogs = () => setProps({...props, logData: []});
+
+    const handleSettingsClose = () => setProps({...props, showSettings: false});
     const handleSettingsSave = (data: SettingsData) => {
-        setProps({...props, showSettings: false})
-        addLog({level: LogLevel.INFO, data: `set apiUrl to ${data.apiUrl}`});
-        props.localStorage.set("apiUrl", data.apiUrl);
-        addLog({level: LogLevel.INFO, data: `set apiKey to ${data.apiKey}`});
-        props.localStorage.set("apiKey", data.apiKey);
-    }
+        setProps({...props, showSettings: false});
+        logger.info('saving settings:');
+        logger.info(data);
+        props.localStorage.set('apiUrl', data.apiUrl);
+        props.localStorage.set('apiKey', data.apiKey);
+    };
 
     return (
         <div className={styles.App}>
             <Header {...props} onClick={navbarHandler}/>
-            {props.selectedActive && <TagList title={"selected tags"} onSelect={selectTag}/>}
-            {props.availableActive && <TagList title={"available tags"} onSelect={deselectTag}/>}
+            {props.selectedActive && <TagList title={'selected tags'} onSelect={selectTag}/>}
+            {props.availableActive && <TagList title={'available tags'} onSelect={deselectTag}/>}
             <Bookmarks bookmarks={props.bookmarks} onEdit={editBookmark} onDelete={deleteBookmark}/>
             {props.logsActive && <Logs logs={props.logData} onClear={clearLogs}/>}
 
-            {props.showSettings && <Settings data={{
-                apiUrl: props.localStorage.get("apiUrl") || "",
-                apiKey: props.localStorage.get("apiKey") || ""
-            }}
-                                             handleClose={handleSettingsClose} handleSave={handleSettingsSave}/>}
+            {props.showSettings && <Settings
+                data={{
+                    apiUrl: props.localStorage.get('apiUrl') || '',
+                    apiKey: props.localStorage.get('apiKey') || ''
+                }}
+                handleClose={handleSettingsClose}
+                handleSave={handleSettingsSave}/>}
         </div>
     );
 }
---
+++ frontend/src/logs/Logger.ts
@@ -0,0 +1,65 @@
+import {LogData, LogLevel} from './LogData';
+import {LocalStorage} from '../localstorage/LocalStorage';
+
+export interface LoggerListener {
+    log: (logData: LogData) => void
+}
+
+export class Logger {
+
+    localStorage: LocalStorage = new LocalStorage();
+
+    constructor(private listener: LoggerListener) {
+    }
+
+    debug = (data: any) => this.log(new LogData(LogLevel.DEBUG, data));
+    info = (data: any) => this.log(new LogData(LogLevel.INFO, data));
+    warn = (data: any) => this.log(new LogData(LogLevel.WARN, data));
+    error = (data: any) => this.log(new LogData(LogLevel.ERROR, data));
+
+    private log = (logData: LogData) => {
+        const logLevel: LogLevel = this.logLevel(this.localStorage.get('logLevel') || 'INFO');
+        if (this.logLevelNum(logData.level) >= this.logLevelNum(logLevel)) {
+            this.listener.log(logData);
+        }
+    };
+
+    private logLevel(s: string): LogLevel {
+        let logLevel = LogLevel.INFO;
+        switch (s) {
+            case 'DEBUG':
+                logLevel = LogLevel.DEBUG;
+                break;
+            case 'INFO':
+                logLevel = LogLevel.INFO;
+                break;
+            case 'WARN':
+                logLevel = LogLevel.WARN;
+                break;
+            case 'ERROR':
+                logLevel = LogLevel.ERROR;
+                break;
+        }
+        return logLevel;
+    }
+
+    private logLevelNum(logLevel: LogLevel): number {
+        let num = -1;
+        switch (logLevel) {
+            case LogLevel.DEBUG:
+                num = 0;
+                break;
+            case LogLevel.INFO:
+                num = 1;
+                break;
+            case LogLevel.WARN:
+                num = 2;
+                break;
+            case LogLevel.ERROR:
+                num = 3;
+                break;
+        }
+        return num;
+    }
+}
+
