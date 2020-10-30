60e7afdd93be74f41cd680eeff2be8d190e4cc8d
P.J. Meisch <pj.meisch@sothawo.com>
2020-10-30 08:14:33 +0100

    get all tags from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import {TagList} from './taglist/TagList';
 import {Logs} from './logs/Logs';
 import {Bookmarks} from './bookmarks/Bookmarks';
 import {Bookmark} from './bookmarks/Bookmark';
-import {LogData, LogLevel} from './logs/LogData';
+import {LogData} from './logs/LogData';
 import {LocalStorage} from './localstorage/LocalStorage';
 import {Settings, SettingsData} from './settings/Settings';
 import {Logger} from './logs/Logger';
@@ -17,6 +17,8 @@ export interface AppProps {
     selectedActive: boolean,
     logsActive: boolean,
     showSettings: boolean,
+    availableTags: string[],
+    selectedTags: string[],
     bookmarks: Bookmark[],
     logData: LogData[],
     localStorage: LocalStorage
@@ -27,7 +29,9 @@ const initialAppProps: AppProps = {
     selectedActive: true,
     logsActive: false,
     showSettings: false,
-    bookmarks: [new Bookmark('42', 'https://www.sothawo.com', 'sothawo-website', ['cool', 'unbelievable'])],
+    availableTags: [],
+    selectedTags: [],
+    bookmarks: [],
     logData: [],
     localStorage: new LocalStorage()
 };
@@ -55,21 +59,41 @@ export function App() {
 
     const [props, setProps] = useState<AppProps>(initialAppProps);
 
-    const backend = new Backend();
+    const logger = new Logger({
+        log: (logData: LogData) => {
+            props.logData.push(logData);
+            setProps({...props});
+        }
+    });
+
+    const backend = new Backend(props.localStorage, logger);
 
     const navbarHandler = (id: string) => {
         console.log(`Header event with id: ${id}`);
         switch (id) {
+            case 'init-app':
+                initApp()
+                break;
             case 'settings':
                 setProps({...props, showSettings: true});
-                break;
-            case 'init-app':
-                backend.getIp().subscribe(value => logger.info(value));
                 break;
             default:
                 setProps(toggleComponentAvailability(props, id));
                 break;
         }
+    };
+
+    const initApp = () => {
+        backend.allTags().subscribe(allTags => {
+            logger.debug(`<- data:`);
+            logger.debug(allTags);
+            const newProps = {
+                ...props,
+                availableTags: allTags,
+                selectedTags: []
+            };
+            setProps(newProps);
+        });
     };
 
     const selectTag = (tag: string) => {
@@ -80,13 +104,6 @@ export function App() {
     };
     const deleteBookmark = (id: string) => {
     };
-
-    const logger = new Logger({
-        log: (logData: LogData) => {
-            props.logData.push(logData);
-            setProps({...props});
-        }
-    });
 
     const clearLogs = () => setProps({...props, logData: []});
 
@@ -103,8 +120,8 @@ export function App() {
     return (
         <div className={styles.App}>
             <Header {...props} onClick={navbarHandler}/>
-            {props.selectedActive && <TagList title={'selected tags'} onSelect={selectTag}/>}
-            {props.availableActive && <TagList title={'available tags'} onSelect={deselectTag}/>}
+            {props.selectedActive && <TagList title={'selected tags'} tags={props.selectedTags} onSelect={selectTag}/>}
+            {props.availableActive && <TagList title={'available tags'} tags={props.availableTags} onSelect={deselectTag}/>}
             <Bookmarks bookmarks={props.bookmarks} onEdit={editBookmark} onDelete={deleteBookmark}/>
             {props.logsActive && <Logs logs={props.logData} onClear={clearLogs}/>}
 
--- frontend/src/backend/Backend.ts
+++ frontend/src/backend/Backend.ts
@@ -1,6 +1,8 @@
 import {Observable, of} from 'rxjs';
 import {fromFetch} from 'rxjs/fetch';
-import {mergeMap, switchMap} from 'rxjs/operators';
+import {mergeMap} from 'rxjs/operators';
+import {LocalStorage} from '../localstorage/LocalStorage';
+import {Logger} from '../logs/Logger';
 
 interface IpApi {
     query: string
@@ -8,13 +10,51 @@ interface IpApi {
 
 export class Backend {
 
-    getIp = (): Observable<string> => {
-        const responseObservable = fromFetch('http://ip-api.com/json');
-        const observable = responseObservable.pipe<any, IpApi, string>(
+    constructor(private localStorage: LocalStorage, private logger: Logger) {
+    }
+
+
+    // todo remove
+    getIp(): Observable<string> {
+        return fromFetch('http://ip-api.com/json').pipe<any, IpApi, string>(
             mergeMap(response => response.json()),
             mergeMap(foo => of((foo as IpApi))),
             mergeMap(ipApi => of(ipApi.query))
         );
-        return observable;
     };
+
+    allTags(): Observable<string[]> {
+        return fromFetch(this.buildRequest('GET', '/tags'))
+            .pipe(mergeMap(response => response.json()));
+    }
+
+    private buildRequest(method: string, path: string): Request {
+        const request: Request = new Request(
+            `${this.localStorage.get('apiUrl')}${path}`,
+            {
+                method: method,
+                headers: this.getHeaders()
+            });
+        this.preCallLogging(request);
+        return request;
+    }
+
+    private getHeaders(): Headers {
+        return new Headers({
+            'Accept': 'application/json',
+            'X-Api-Key': `${this.localStorage.get('apiKey')}`
+        });
+    }
+
+    private preCallLogging(request: Request) {
+
+        this.logger.info(`-> ${request.method} ${request.url}`);
+        request.headers.forEach((v, k) => {
+            this.logger.debug(`-> ${k}: ${v}`);
+        });
+
+        if (request.body) {
+            this.logger.debug(request.body);
+        }
+    }
 }
